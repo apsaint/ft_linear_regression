1c5df0f1522ad709b5244530e13cf642ff419443
apsaint- <apsaint-@student.42.fr>
2020-01-08 10:50:29 +0100

    Implement gradient descent

--- entrainement.py
+++ entrainement.py
@@ -49,31 +49,33 @@ def read_data(file) -> list:
     return list_csv
 
 
-def lr_theta0(csv_reader, theta0, theta1) -> float:
-
+def partial_derivative(csv_reader, theta0, theta1, m) -> (float, float):
     dth0 = 0.0
-    for row in csv_reader:
-        dth0 += (theta0 + (theta1 * float(row['km'])) - float(row['price']))
-    return float(dth0)
-
-
-def lr_theta1(csv_reader, theta0, theta1) -> float:
     dth1 = 0.0
     for row in csv_reader:
+        dth0 += (theta0 + (theta1 * float(row['km'])) - float(row['price']))
         dth1 += (theta0 + (theta1 * float(row['km'])) - float(row['price'])) * float(row['km'])
-    return float(dth1)
+    dth0 = (1.0/m) * dth0
+    dth1 = (1.0/m) * dth1
+    return (float(dth0), float(dth1))
 
 
-def lr_derivee(csv_reader, theta0, theta1) -> (float, float):
-    tmp0 = 0.0
-    tmp1 = 0.0
+def calc_new_thetas(csv_reader, theta0, theta1) -> (float, float):
     m = len(csv_reader)
     learningRate = 0.1
-    d0 = lr_theta0(csv_reader, theta0, theta1)
-    d1 = lr_theta1(csv_reader, theta0, theta1)
-    tmp0 = theta0 - (learningRate * (d0 / m))
-    tmp1 = theta1 - (learningRate * (d1 / m))
-    return (tmp0, tmp1)
+    (d0, d1) = partial_derivative(csv_reader, theta0, theta1, m)
+    new_theta0 = theta0 - (learningRate * d0)
+    new_theta1 = theta1 - (learningRate * d1)
+    return (new_theta0, new_theta1)
+
+
+def start_gradient_descent(csv_reader, theta0, theta1) -> (float, float):
+    tmpth0 = theta0
+    tmpth1 = theta1
+    for r in csv_reader:
+        new_theta0, new_theta1 = calc_new_thetas(csv_reader, tmpth0, tmpth1)
+        tmpth0, tmpth1 = new_theta0, new_theta1
+    return ((tmpth0, tmpth1))
 
 
 def get_max(csv_reader, key) -> float:
@@ -84,13 +86,23 @@ def get_max(csv_reader, key) -> float:
     return max
 
 
+def get_min(csv_reader, key) -> float:
+    min = 123456.0
+    for r in csv_reader:
+        if float(r[key]) < min:
+            min = float(r[key])
+    return min
+
+
 def normalise_data(csv_reader) -> list:
     norm_data = []
     max_km = get_max(csv_reader, 'km')
+    min_km = get_min(csv_reader, 'km')
     max_price = get_max(csv_reader, 'price')
+    min_price = get_min(csv_reader, 'price')
     for row in csv_reader:
-        row['km'] = float(row['km']) / max_km
-        row['price'] = float(row['price']) / max_price
+        row['km'] = (float(row['km']) - min_km) / (max_km - min_km)
+        row['price'] = (float(row['price']) - min_price) / (max_price - min_price)
         norm_data.append(row)
     return norm_data
 
@@ -101,9 +113,7 @@ if __name__ == "__main__":
     for row in th:
         th0 = float(row['theta0'])
         th1 = float(row['theta1'])
-    norm_data = normalise_data(csv_reader.copy())
-    theta0, theta1 = lr_derivee(norm_data, th0, th1)
-    theta0 = theta0 * get_max(csv_reader, 'km')
-    theta1 = theta1 * (get_max(csv_reader, 'price') / get_max(csv_reader, 'km'))
+    #norm_data = normalise_data(csv_reader.copy())
+    theta0, theta1 = start_gradient_descent(csv_reader, th0, th1)
     p = estimatePrice_calc(240000, theta0, theta1)
     print(p)
